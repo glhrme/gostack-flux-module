7763d3699d54ef5d31d886a7545041fe0d7679b1
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-01-03 16:42:54 -0300

    Estilização Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { MdShoppingBasket } from 'react-icons/md'
+
+import { Link } from 'react-router-dom'
+
+import { Container, Cart } from './style'
+
+import logo from '../../assets/images/logo.svg'
+
+export default function index() {
+    return (
+        <Container>
+            <Link to='/'>
+                <img src={logo} alt='RocketShoes'/>
+            </Link>
+
+            <Cart to='/cart'>
+                <div>
+                    <strong>Meu carrinho</strong>
+                    <span>3 itens</span>
+                </div>
+                <MdShoppingBasket size={36}/>
+            </Cart>
+        </Container>
+    )
+}
